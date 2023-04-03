77b91ce42abfa5e635ca01ba13e2339ed2e1b918
gguditus <126524383+gguditus@users.noreply.github.com>
2023-04-03 13:29:10 -0400

    added reload buttons, hard mode, and images

--- RPS.js
+++ RPS.js
@@ -1,5 +1,19 @@
 let playerScore = 0;
 let compScore = 0;
+let playerWonEasy;
+
+let playerMoveFreq = { Rock: 0, Paper: 0, Scissors: 0 };
+
+// Retrieve stored data from session storage
+if (sessionStorage.getItem("playerWonEasy")) {
+  playerWonEasy = JSON.parse(sessionStorage.getItem("playerWonEasy"));
+} else {
+  playerWonEasy = false;
+}
+
+if (sessionStorage.getItem("playerMoveFreq")) {
+  playerMoveFreq = JSON.parse(sessionStorage.getItem("playerMoveFreq"));
+}
 
 const playerBtns = document.querySelectorAll("input");
 
@@ -9,11 +23,12 @@ function checkScore() {
 }
 /*returns the appropriate result when the player wins */
 function playerWin() {
-  return "</br>You won the game! Reload the page to play again";
+  playerWonEasy = true;
+  return "</br>You won the game! </br><button onclick='location.reload()'>Play Hard Mode</button>";
 }
 /*returns the appropriate result when the computer wins */
 function compWin() {
-  return "</br>You lost the game. :( Reload the page to play again";
+  return "</br>You lost the game. :( </br><button onclick='location.reload()'>Play Again</button>";
 }
 
 /* checks whose score is greater and returns the result from each respective function*/
@@ -27,10 +42,38 @@ function endGame() {
     return compWin();
   }
 }
-/*randomly selects a choice for the computer and returns it*/
+
+/*randomly selects a move for the computer and returns it, if the player has won easy, use the hard mode function instead*/
 function compSelect() {
-  let choice = ["Rock", "Paper", "Scissors"];
-  return choice[Math.floor(Math.random() * choice.length)];
+  if (playerWonEasy) {
+    return hardCompSelect();
+  } else {
+    let moves = ["Rock", "Paper", "Scissors"];
+    return moves[Math.floor(Math.random() * moves.length)];
+  }
+}
+
+/*hard mode function, main difference here. takes into account the frequency the player chose each move*/
+function hardCompSelect() {
+  let mostFrequentMove = Object.keys(playerMoveFreq).reduce((a, b) =>
+    playerMoveFreq[a] > playerMoveFreq[b] ? a : b
+  );
+
+  let counterMove;
+  if (mostFrequentMove === "Rock") {
+    counterMove = "Paper";
+  } else if (mostFrequentMove === "Paper") {
+    counterMove = "Scissors";
+  } else {
+    counterMove = "Rock";
+  }
+
+  if (playerMoveFreq[mostFrequentMove] == 0) {
+    let moves = ["Rock", "Paper", "Scissors"];
+    let random = Math.floor(Math.random() * 3);
+    counterMove = moves[random];
+  }
+  return counterMove;
 }
 
 /*returns player selection if player won, otherwise returns computer selection*/
@@ -95,13 +138,19 @@ function playGame(playerSelection) {
     /*otherwise gets the result of the round*/
     result = getResult(playerSelection, compSelection);
   }
+
+  playerMoveFreq[playerSelection] = playerMoveFreq[playerSelection] + 1;
+
   document.getElementById("result").innerHTML = result;
 
   if (checkScore()) {
     document.getElementById("gameResult").innerHTML = endGame();
+
+    // Store the updated values in sessionStorage
+    sessionStorage.setItem("playerMoveFreq", JSON.stringify(playerMoveFreq));
+    sessionStorage.setItem("playerWonEasy", JSON.stringify(playerWonEasy));
   }
 }
-
 playerBtns.forEach((playerBtn) => {
   playerBtn.addEventListener("click", function () {
     playGame(playerBtn.value);
